ed7c22f159fcee6d9a10b5bc6ea10a77c6090526
jjun990908 <qkrwnsdud@kookmin.ac.kr>
2023-01-27 20:24:15 +0900

    styled component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+`;
+
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  } 
+  50% {
+    border-radius:100px;
+  } 
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} 1s linear infinite;
+  ${Emoji}:hover {
+      font-size: 100px;
+    }
+`;
+
 function App() {
-  return null;
+  return (
+    <Wrapper>
+      <Box>
+        <Emoji>😁</Emoji>
+      </Box>
+      <Emoji>😅</Emoji>
+    </Wrapper>
+  );
 }
 
 export default App;
